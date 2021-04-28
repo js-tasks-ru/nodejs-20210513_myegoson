d7eda73ea2bf7ab8ba4d2a107128d8cc198c353b
den <denis.filipkin@e-champs.com>
2021-04-28 20:30:49 +0400

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,6 @@
 function sum(a, b) {
-  /* ваш код */
+  Array.from(arguments).map(item => !Number.isFinite(item) && (() => {throw new TypeError('Some argument is not a number')})())
+  return a + b;
 }
 
 module.exports = sum;
