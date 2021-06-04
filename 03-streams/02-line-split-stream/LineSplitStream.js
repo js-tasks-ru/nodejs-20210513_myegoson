dcbe79a222a9e152405daa5b4662c97eaf39a031
den <myegoson@gmail.com>
2021-06-04 21:29:57 +0400

    со стримами все страшно

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,23 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.buffer = []
   }
 
   _transform(chunk, encoding, callback) {
+    const string = chunk.toString()
+    const stringArray = string.split(os.EOL)
+    console.log(stringArray.length)
+    stringArray.forEach(item => {
+      this.push(item)
+    })
+
+
+    callback()
   }
 
   _flush(callback) {
+    callback(null)
   }
 }
 
