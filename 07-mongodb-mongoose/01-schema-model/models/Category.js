1df3e1c6ddf90c3a0f14bacca6ce5694afafdfc8
dasha <kykolka_1992@mail.ru>
2021-06-05 16:10:51 +0400

    models

--- 07-mongodb-mongoose/01-schema-model/models/Category.js
+++ 07-mongodb-mongoose/01-schema-model/models/Category.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
+const Schema = mongoose.Schema;
 
 const subCategorySchema = new mongoose.Schema({
+  _id: Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
 });
 
 const categorySchema = new mongoose.Schema({
+  _id: Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
   subcategories: [subCategorySchema],
 });
 
