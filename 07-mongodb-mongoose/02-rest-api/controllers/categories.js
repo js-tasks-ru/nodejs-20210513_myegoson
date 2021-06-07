b3770c6454afe9909c7ea5d93d22c0a3e2c04cbe
dasha <kykolka_1992@mail.ru>
2021-06-07 23:37:06 +0400

    mongoose

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -2,4 +2,5 @@ const Category = require('../models/Category');
 
 module.exports.categoryList = async function categoryList(ctx, next) {
   ctx.body = {categories: await Category.find({})};
+  next()
 };
--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,18 +1,28 @@
 const Product = require('../models/Product');
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  console.log(ctx.request.query);
   const {subcategory} = ctx.request.query;
+  console.log(ctx.request.query); // тут пусто =( и тут проходит тест
+  // 'если товаров не найдено - должен возвращаться пустой массив'
   const products = await Product.find({subcategory: subcategory});
+  ctx.body = {products: products};
+  next();
+};
+
+module.exports.productList = async function productList(ctx, next) {
+  console.log(await Product.find({}));
+  const products = await Product.find({});
   if (products.length === 0) ctx.body = {products: []};
   else ctx.body = {products: products};
 };
 
-module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
-};
-
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  console.log(ctx.request.query.id);
+  if (ctx.request.query.id instanceof Product.Types.ObjectId ) {
+    ctx.body = {product: await Product.find({id: ctx.request.params.id})};
+  } else {
+    ctx.response.status=400;
+    throw new Error();
+  }
 };
 
