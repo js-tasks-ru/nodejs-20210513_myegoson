d35189a20ac980a5b1e21f156bf0bd344c152296
dasha <kykolka_1992@mail.ru>
2021-06-12 15:15:25 +0400

    social strategies

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,10 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  const user = User.findOne({email: email});
+  if (user) done(null, user);
+  else {
+    const newUser = await User.create({email: email, displayName: displayName});
+    done(null, newUser);
+  }
 };
